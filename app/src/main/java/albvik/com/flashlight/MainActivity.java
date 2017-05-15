45b4386a3af8277d62249eab7902d4d5b6a9b2b8
albvik <albvik@gmail.com>
2017-05-15 13:31:46 +0300

    Initial commit

---
+++ app/src/main/java/albvik/com/flashlight/MainActivity.java
@@ -0,0 +1,185 @@
+package albvik.com.flashlight;
+
+import android.annotation.TargetApi;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.Image;
+import android.media.SoundPool;
+import android.net.UrlQuerySanitizer;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.Switch;
+
+import java.io.IOException;
+import java.security.Policy;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements  SoundPool.OnLoadCompleteListener{
+    private int sound;
+    private SoundPool soundPool;
+    private Camera camera;
+    Camera.Parameters parameters;
+    private Switch mySwitch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Lollipop) {
+            createSoundPoolWithBuilder();
+        } else {
+            createSoundPoolWithConstructor();
+        }
+
+        soundPool.setOnLoadCompleteListener(this);
+        sound = soundPool.load(this, R.raw.click, 1);
+
+
+
+        mySwitch = (Switch) findViewById(R.id.my_switch);
+        mySwitch.setChecked(true);
+        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    setFlashLigthOn();
+                } else {
+                    setFlashLightOff();
+                }
+            }
+        });
+
+        boolean isCameraFlash = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if (!isCameraFlash) {
+            showCameraAlert();
+        } else {
+            camera = Camera.open();
+        }
+
+    }
+    private void showCameraAlert() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.error_title)
+                .setMessage(R.string.error_text)
+                .setPositiveButton(R.string.exit_message, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    protected void createSoundPoolWithBuilder() {
+        AudioAttributes attributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+
+        soundPool = new SoundPool.Builder().setAudioAttributes(attributes).setMaxStreams(1).build();
+    }
+
+    @SuppressWarnings("deprecation")
+    protected void createSoundPoolWithConstructor() {
+        soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+    }
+
+    private void setFlashLigthOn() {
+        soundPool.play(sound, 1, 1, 0, 0, 1);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (camera != null) {
+                    parameters = camera.getParameters();
+
+                    if (parameters != null) {
+                        List supportedFlashModes = parameters.getSupportedFlashModes();
+
+                        if (supportedFlashModes.contains(Camera.Parameters.FLASH_MODE_TORCH)) {
+                            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                        } else if (supportedFlashModes.contains(Camera.Parameters.FLASH_MODE_ON)) {
+                            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_ON);
+                        } else camera = null;
+
+                        if (camera != null) {
+                            camera.setParameters(parameters);
+                            camera.startPreview();
+                            try {
+                                camera.setPreviewTexture(new SurfaceTexture(0));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void setFlashLightOff() {
+        soundPool.play(sound, 1, 1, 0, 0, 1);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (camera != null) {
+                    parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                    camera.setParameters(parameters);
+                    camera.stopPreview();
+                }
+            }
+        }).start();
+    }
+
+    private void releaseCamera() {
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();
+        mySwitch.setChecked(false);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (camera == null) {
+            camera = Camera.open();
+        } else{
+            setFlashLigthOn();
+        }
+        mySwitch.setChecked(true);
+    }
+
+    @Override
+    public void onLoadComplete(SoundPool soundPool, int i, int i1) {
+
+    }
+}
